25c4471dd733ac0648a7957f9fa95d9a35891c16
DONGURAN <>
2024-10-17 16:36:33 +0900

    Change Android Plugin name. 'Aes256cipherPlugin' → 'AES256CipherPlugin'

--- android/src/main/kotlin/com/donguran/aes256cipher/AES256CipherPlugin.kt
+++ android/src/main/kotlin/com/donguran/aes256cipher/AES256CipherPlugin.kt
@@ -10,7 +10,7 @@ import io.flutter.plugin.common.MethodChannel.Result
 import kotlinx.coroutines.*
 
 /** Aes256cipherPlugin */
-class Aes256cipherPlugin: FlutterPlugin, MethodCallHandler {
+class AES256CipherPlugin: FlutterPlugin, MethodCallHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
